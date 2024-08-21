a681ad604984966bf69a5907700ead9cb73c87ad
KurubaN770266 <shikharnarain2@gmail.com>
2024-08-21 09:35:27 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -25,3 +25,31 @@ void check_for_negatives(const vector<int> &numbers) {
 		throw invalid_argument("Negatives not allowed: " + negatives);
 	}
 }
+
+void add_if_valid(vector<int> &numbers, string number_string) {
+	int number = stoi(number_string);
+	if (number <= 1000)
+		numbers.push_back(number);
+}
+
+vector<int> get_numbers(const string &input) {
+	regex numbers_only("(-?\\d+)+");
+
+	vector<int> numbers;
+	for_each(sregex_token_iterator(input.begin(), input.end(), numbers_only),
+				sregex_token_iterator(),
+				[&numbers](string s) { add_if_valid(numbers, s); });
+
+	return numbers;
+}
+
+int StringCalculator::Add(const string &input) {
+	if (input.empty())
+		return 0;
+
+	vector<int> numbers = get_numbers(input);
+
+	check_for_negatives(numbers);
+
+	return accumulate(numbers.begin(), numbers.end(), 0);
+}
